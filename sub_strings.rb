b5ef0d0bda5216e060919e9752f696ca1cd229c8
Mindy Johnson <mindy.l.brock@gmail.com>
2021-08-11 21:05:58 -0500

    Created ruby file and skeleton of substrings method

---
+++ sub_strings.rb
@@ -0,0 +1,8 @@
+def substrings(text, dictionary)
+  p text
+  p dictionary
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+substrings("Howdy!", dictionary)
